56127f1c713ffaa262d7081144efbf24ff4c398e
Chinmay <chinmay.codesworld@gmail.com>
2024-02-28 14:47:35 +0530

    Chapter 4

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,13 @@
 
-
+import Chai from './chai.jsx'
 
 function App() {
 
   return (
+    <>
+    <Chai />
     <h1>Chai aur React | Vite</h1>
+    </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const newContainer = (
+  <a href="https://google.com" target='_blank'>Click me </a>
+)
+
+const customContainer = React.createElement(
+  'a',
+  {href: 'http://google.com',target: '_blank'},
+  'click to go to google'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  customContainer
 )
