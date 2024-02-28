9387e80244b5cf4de1fb04d0e444026140e75474
Chinmay <chinmay.codesworld@gmail.com>
2024-02-28 19:25:54 +0530

    Module 5

---
+++ 02counterProject/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0)
+
+ 
+
+  function increase () {
+    if(counter < 20){
+      setCounter(++counter)
+    }
+  }
+  function decrease () {
+    if(counter != 0){
+      setCounter(--counter)
+    }
+  }
+
+  return (
+    <>
+    <h1>Chai aur react</h1>
+    <h2>Counter Value: {counter}</h2>
+
+    <button onClick={increase}>Increase Value</button>
+    <br />
+    <button onClick={decrease}>Decrease Value</button>
+    </>
+  )
+}
+
+export default App
